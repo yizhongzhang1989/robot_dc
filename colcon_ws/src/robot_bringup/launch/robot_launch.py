78bdac46abd92f560c0f3aca2a9cee9e81e76578
Yizhong Zhang <yizhongzhang1989@gmail.com>
2025-05-20 19:52:44 +0800

    added launch web into launch robot

--- colcon_ws/src/robot_bringup/launch/robot_launch.py
+++ colcon_ws/src/robot_bringup/launch/robot_launch.py
@@ -4,52 +4,53 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
+
 def generate_launch_description():
-    # Path to modbus_driver launch
-    modbus_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('modbus_driver'),
-                'launch',
-                'modbus_manager_launch.py'
-            )
-        )
+    # Paths to launch files
+    modbus_path = os.path.join(
+        get_package_share_directory('modbus_driver'),
+        'launch',
+        'modbus_manager_launch.py'
+    )
+    motor_path = os.path.join(
+        get_package_share_directory('leadshine_motor'),
+        'launch',
+        'motor_control_launch.py'
+    )
+    teleop_path = os.path.join(
+        get_package_share_directory('robot_teleop'),
+        'launch',
+        'joystick_teleop_launch.py'
+    )
+    web_path = os.path.join(
+        get_package_share_directory('robot_web'),
+        'launch',
+        'web_server_launch.py'
     )
 
-    # Delay motor node launch by 2 seconds
+    # Launch descriptions
+    modbus_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(modbus_path)
+    )
+
     motor_launch = TimerAction(
         period=2.0,
-        actions=[
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(
-                        get_package_share_directory('leadshine_motor'),
-                        'launch',
-                        'motor_control_launch.py'
-                    )
-                )
-            )
-        ]
+        actions=[IncludeLaunchDescription(PythonLaunchDescriptionSource(motor_path))]
     )
 
-    # Delay teleop launch by 3 seconds (to ensure motor nodes are ready)
     teleop_launch = TimerAction(
         period=3.0,
-        actions=[
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(
-                        get_package_share_directory('robot_teleop'),
-                        'launch',
-                        'joystick_teleop_launch.py'
-                    )
-                )
-            )
-        ]
+        actions=[IncludeLaunchDescription(PythonLaunchDescriptionSource(teleop_path))]
+    )
+
+    web_launch = TimerAction(
+        period=1.0,
+        actions=[IncludeLaunchDescription(PythonLaunchDescriptionSource(web_path))]
     )
 
     return LaunchDescription([
         modbus_launch,
         motor_launch,
-        teleop_launch
+        teleop_launch,
+        web_launch
     ])
