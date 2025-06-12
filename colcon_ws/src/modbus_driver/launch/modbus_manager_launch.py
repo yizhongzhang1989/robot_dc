1499944082dba5a19b3d51b06e87b7dc855c300d
Yizhong Zhang <yizhongzhang1989@gmail.com>
2025-06-12 17:12:16 +0800

    changed serial port id

--- colcon_ws/src/modbus_driver/launch/modbus_manager_launch.py
+++ colcon_ws/src/modbus_driver/launch/modbus_manager_launch.py
@@ -8,7 +8,7 @@ def generate_launch_description():
             executable='modbus_manager_node',
             name='modbus_manager_node',
             parameters=[
-                {'port': '/dev/ttyUSB1'},
+                {'port': '/dev/ttyUSB0'},
                 {'baudrate': 115200}
             ],
             output='screen'
