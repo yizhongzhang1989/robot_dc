4382723e41409314b1911ff5e7c833789f8eae02
Yizhong Zhang <yizhongzhang1989@gmail.com>
2025-07-11 14:56:49 +0800

    Remove install directory from repository and add to .gitignore

--- install/modbus_driver_interfaces/local/lib/python3.10/dist-packages/modbus_driver_interfaces/srv/__init__.py
+++
@@ -1,1 +0,0 @@
-from modbus_driver_interfaces.srv._modbus_request import ModbusRequest  # noqa: F401
--- install/modbus_driver_interfaces/share/modbus_driver_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(modbus_driver_interfaces_IDL_FILES "srv/ModbusRequest.idl;msg/ModbusPacket.idl;msg/MotorSimulationStatus.idl")
-set(modbus_driver_interfaces_INTERFACE_FILES "srv/ModbusRequest.srv;srv/ModbusRequest_Request.msg;srv/ModbusRequest_Response.msg;msg/ModbusPacket.msg;msg/MotorSimulationStatus.msg")
