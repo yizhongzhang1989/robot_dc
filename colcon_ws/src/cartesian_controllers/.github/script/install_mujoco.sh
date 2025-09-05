2daf645a7fe083e9345e1e9e725cee92bf1f382c
yizhongzhang <yizhongzhang1989@gmail.com>
2025-09-05 18:50:09 +0800

    Add official cartesian controllers and fix configuration
    - Added official cartesian_controllers from fzi-forschungszentrum-informatik
    - Fixed cartesian_controller_manager.yaml: uncommented controller types and changed end_effector_link from tool0 to link_6
    - Removed target_frame_publisher.py script as it's no longer needed for manual control
    - System now supports manual cartesian space robot control via /target_frame topic

---
+++ colcon_ws/src/cartesian_controllers/.github/script/install_mujoco.sh
@@ -0,0 +1,5 @@
+#/usr/bin/bash
+cd $HOME
+apt install wget
+wget https://github.com/deepmind/mujoco/releases/download/3.0.0/mujoco-3.0.0-linux-x86_64.tar.gz
+tar -xf mujoco-3.0.0-linux-x86_64.tar.gz
