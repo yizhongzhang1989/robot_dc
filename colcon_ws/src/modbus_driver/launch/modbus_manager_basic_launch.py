f8448c04a84f88e0bdf7283a9cc8fb1ca9ba6acc
yizhongzhang1989 <yizhongzhang1989@gmail.com>
2025-09-16 22:25:21 +0800

    feat(modbus_driver): add basic modbus_manager_basic_launch without serial parameters

---
+++ colcon_ws/src/modbus_driver/launch/modbus_manager_basic_launch.py
@@ -0,0 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='modbus_driver',
+            executable='modbus_manager_node',
+            name='modbus_manager_node',
+            output='screen'
+        )
+    ])
